f6db6a32d4b9291bb48f0a400fee567cd9e10ff8
Adolfo Lozano Mendez <gadolfolozano@gmail.com>
2020-04-28 18:58:03 -0300

    creating card view example

--- app/src/main/java/pe/gadolfolozano/composeexperiment/MainActivity.kt
+++ app/src/main/java/pe/gadolfolozano/composeexperiment/MainActivity.kt
@@ -3,11 +3,14 @@ package pe.gadolfolozano.composeexperiment
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
-import androidx.ui.core.Text
 import androidx.ui.core.setContent
+import androidx.ui.foundation.Text
+import androidx.ui.foundation.shape.corner.RoundedCornerShape
 import androidx.ui.layout.Column
+import androidx.ui.material.Card
 import androidx.ui.material.MaterialTheme
 import androidx.ui.tooling.preview.Preview
+import androidx.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,11 +25,14 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun Story(name: String) {
-    Column {
-        Text(text = "Hello $name!")
-        Text(text = "What you doing?")
-        Text(text = "I am trying out some compose stuff")
+    Card(shape = RoundedCornerShape(4.dp), elevation = 4.dp) {
+        Column {
+            Text(text = "Hello $name!")
+            Text(text = "What you doing?")
+            Text(text = "I am trying out some compose stuff")
+        }
     }
+
 }
 
 @Preview
